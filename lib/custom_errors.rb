0beb23b6d7a5dc8684bc8eec7598005f9ecf06ca
Martin Achtermann <mdacht777@gmail.com>
2020-07-05 15:45:12 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -5,10 +5,10 @@ class Person
     @name = name
   end
 
-  def get_married(person)
-    self.partner = person
-    person.partner = self
-  end
+  # def get_married(person)
+  #   self.partner = person
+  #   person.partner = self
+  # end
 class PartnerError < StandardError
 end
 end
